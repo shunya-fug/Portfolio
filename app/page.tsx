0abcf2e202f1a3c7a7f00a295b0678e9cdb684d8
shunya-fug <shunnya0715@gmail.com>
2023-07-02 08:35:07 +0000

    🔨：データ取得処理を修正
    キャッシュを利用しない。

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@ import { client } from "@/src/lib/client"
 import type { Blog } from "@/src/types/microcms/blog"
 
 export default async function Home() {
-  const response = await client.get({ endpoint: "blogs" })
+  const response = await client.getList<Blog>({
+    endpoint: "blogs",
+    customRequestInit: {
+      cache: "no-store"
+    }
+  })
 
   return (
     <main>
-      {response.contents.map((blog: Blog) => (
+      {response.contents.map((blog) => (
         <div key={blog.id}>
           <h1>{blog.title}</h1>
           <p>{blog.publishedAt}</p>
-          <div dangerouslySetInnerHTML={{__html: blog.content}} />
-          <p>{blog.category.name}</p>
+          <p>{blog.category && blog.category.name}</p>
         </div>
       ))}
     </main>
